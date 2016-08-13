f4f72cfe83ab80521f3b8346c0bb01cacdf4bc9a
Manjunatha <ymanju1989@gmail.com>
2016-08-13 15:13:44 +0530

    hi

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -15,14 +15,15 @@ public class DemoA {
 	
 	@Test
 	public void testA(XmlTest x) throws MalformedURLException{
-		String browser=x.getParameter("browser");
-		String remoteip=x.getParameter("ipaddr");
-//		System.setProperty("webdriver.gecko.driver", "C:\\Manjunatha\\SelRel\\geckodriver.exe");
-//		URL remote_addr=new URL("http://10.10.4.166:4444/wd/hub");
-		URL remote_addr=new URL(remoteip);
-		DesiredCapabilities dc=new DesiredCapabilities();
-		dc.setBrowserName(browser);
-		WebDriver driver=new RemoteWebDriver(remote_addr,dc);
+//		String browser=x.getParameter("browser");
+//		String remoteip=x.getParameter("ipaddr");
+////		System.setProperty("webdriver.gecko.driver", "C:\\Manjunatha\\SelRel\\geckodriver.exe");
+////		URL remote_addr=new URL("http://10.10.4.166:4444/wd/hub");
+//		URL remote_addr=new URL(remoteip);
+//		DesiredCapabilities dc=new DesiredCapabilities();
+//		dc.setBrowserName(browser);
+		WebDriver driver=new FirefoxDriver();
+//		WebDriver driver=new RemoteWebDriver(remote_addr,dc);
 		driver.get("https://www.google.co.in/");
 		System.out.println(driver.getTitle());
 		
